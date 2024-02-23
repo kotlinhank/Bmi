234a6a51bcc3ddfc8cd4eca74c973052087c46b3
Hank Tom <hanktom@gmail.com>
2024-02-23 13:55:29 +0800

    initial commit

---
+++ app/src/main/java/com/hank/bmi/Hello.kt
@@ -0,0 +1,24 @@
+package com.hank.bmi
+
+class Hello {
+
+}
+
+fun main() {
+    println("Hello Kotlin!")
+    //Byte, Short, Int, Long
+    val age = 20
+    val population = 9999L
+    println(age.inc())
+    println(age)
+    var name = "Hank"
+    println(name.get(1))
+    name = "Jack"
+    println(name)
+    //Float(32), Double(64)
+    val weight:Float = 65.5f
+    //true, false
+    val isAdult = false
+    //Char
+    val c = 'A'
+}
