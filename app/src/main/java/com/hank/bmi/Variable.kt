89a23cf828f696b10efa64abe10d4ab9a653e350
Hank Tom <hanktom@gmail.com>
2024-02-25 14:08:17 +0800

    數值運算與比較

---
+++ app/src/main/java/com/hank/bmi/Variable.kt
@@ -0,0 +1,20 @@
+package com.hank.bmi
+
+fun main() {
+    var counter = 0;
+    counter = counter + 1
+    counter++
+    counter = counter - 1
+    println(counter)
+    println(10 / 3.0)
+    //Boolean
+    var b = false;
+    var a = false
+    println(a && b)
+    println(a || b)
+    //
+    val num = -8
+    println(num <= 5 && num >= -3)
+    //
+    println(num > 5 || num < -3)
+}
