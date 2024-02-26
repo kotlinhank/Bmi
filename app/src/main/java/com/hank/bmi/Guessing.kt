50145f926b3f98193b3c4f06be4e85889c8d401d
Hank Tom <hanktom@gmail.com>
2024-02-26 18:17:29 +0800

    while迴圈 猜數字猜到對

---
+++ app/src/main/java/com/hank/bmi/Guessing.kt
@@ -0,0 +1,21 @@
+package com.hank.bmi
+
+fun main() {
+    val secret = 7
+    var num = 0
+    while (num != secret) {
+        print("Please enter a number(1-10): ")
+        val input = readLine()
+        num = input?.toIntOrNull() ?: 0
+        println("The number you entered: $num")
+        if (num < secret) {
+            println("Bigger")
+        } else if (num > secret) {
+            println("Smaller")
+        } else {
+            println("You got it!")
+
+        }
+    }
+
+}
--- app/src/main/java/com/hank/bmi/Variable.kt
+++ app/src/main/java/com/hank/bmi/Variable.kt
@@ -1,6 +1,10 @@
 package com.hank.bmi
 
 fun main() {
+    var name:String? = "Hank"
+    println(name?.length)
+    println(name ?: 0 )
+
     var counter = 0;
     counter = counter + 1
     counter++
@@ -10,11 +14,11 @@ fun main() {
     //Boolean
     var b = false;
     var a = false
-    println(a && b)
-    println(a || b)
+//    println(a && b)
+//    println(a || b)
     //
     val num = -8
-    println(num <= 5 && num >= -3)
+//    println(num <= 5 && num >= -3)
     //
-    println(num > 5 || num < -3)
+//    println(num > 5 || num < -3)
 }
